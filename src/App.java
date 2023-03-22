44af834bcd5878ed2259a067d705cd4264933b3f
Igor3550 <igormeloigormelo@gmail.com>
2023-03-21 22:48:29 -0300

    Resolution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int x = 2;
+
+        System.out.println("Tabela de multiplicação de " + x);
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + " x " + x + " = " + i*x);
+        }
     }
 }
